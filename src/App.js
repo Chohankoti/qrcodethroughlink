42b16845c13430522cfccd5fa061685f475bb2e1
unknown <chohankoti333@gmail.com>
2023-12-29 23:21:45 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import familyData from "./familyData";
+import QRCode from "qrcode.react";
+import "./App.css";
 
-function App() {
+const App = () => { 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>QR Code with link</h1>
+    <table className="family-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Relationship</th>
+            <th>Gender</th>
+            <th>QR Code</th>
+          </tr>
+        </thead>
+        <tbody>
+          {familyData.map((member) => (
+            <tr key={member.id}>
+              <td>{member.id}</td>
+              <td>{member.name}</td>
+              <td>{member.age}</td>
+              <td>{member.relationship}</td>
+              <td>{member.gender}</td>
+              <td>
+              <QRCode value={`http://localhost:3001/details/${member.id}`} style={{ marginRight: 50 }} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        </table>
+    </>      
   );
-}
+};
 
 export default App;
---
+++ src/familyData.js
@@ -0,0 +1,51 @@
+// data.js
+const familyData = [
+    { id: 1, name: "John Doe", age: 35, relationship: "Father", gender: "Male" },
+    {
+        id: 2,
+        name: "Jane Doe",
+        age: 32,
+        relationship: "Mother",
+        gender: "Female",
+    },
+    {
+        id: 3,
+        name: "Alice Doe",
+        age: 10,
+        relationship: "Daughter",
+        gender: "Female",
+    },
+    { id: 4, name: "Bob Doe", age: 8, relationship: "Son", gender: "Male" },
+    {
+        id: 5,
+        name: "Charlie Doe",
+        age: 28,
+        relationship: "Brother",
+        gender: "Male",
+    },
+    { id: 6, name: "Eva Doe", age: 30, relationship: "Sister", gender: "Female" },
+    {
+        id: 7,
+        name: "David Doe",
+        age: 60,
+        relationship: "Grandfather",
+        gender: "Male",
+    },
+    {
+        id: 8,
+        name: "Grace Doe",
+        age: 58,
+        relationship: "Grandmother",
+        gender: "Female",
+    },
+    {
+        id: 9,
+        name: "Olivia Doe",
+        age: 5,
+        relationship: "Daughter",
+        gender: "Female",
+    },
+    { id: 10, name: "Sam Doe", age: 18, relationship: "Cousin", gender: "Male" },
+];
+
+export default familyData;
